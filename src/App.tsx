28b55969885e328ef9f75063d7ae6a503ee1e9d8
Giorgi Garsevanishvili <George.Garsevanidi@Gmail.com>
2025-08-10 23:20:46 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.scss";
+import IntroIMG from "./assets/Contribo Images Branding/Contribo qirvex fill-no.svg";
+import UseAnimations from "react-useanimations";
+import infinity from "react-useanimations/lib/infinity";
+import { Footer } from "./components/footer-comp/Footer";
+
+function App() {
+  return (
+    <div className="main">
+      <div className="intro-cont"></div>
+      <img className="logo" src={IntroIMG} alt="Contribo Intro" />
+      <UseAnimations
+        className="loading"
+        animation={infinity}
+        size={140}
+        fillColor="#34495e"
+      />
+      <h3 className="intro">We’re cooking up something amazing! </h3>
+      <button className="button-86">See More On Qirvex™</button>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
